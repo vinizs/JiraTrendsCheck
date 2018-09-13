4dcdb3feb47383e1761b940f9766b04426faf09a
Eoin <ecribin@gmail.com>
2018-09-13 11:06:19 +0100

    Uploading python code
    Trending Current Code

---
+++ jira.py
@@ -0,0 +1,106 @@
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+import json
+import base64
+import getpass
+import os
+
+#disable warnings in requests
+#requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+#username is what you are using on machine
+username=os.environ['LOGNAME']
+password=getpass.getpass('Password for '+username+': ')
+
+#aPIbase URL
+jira_server='https://jira2.workday.com/rest/api/2/search?'
+
+#All jiras from https://confluence.workday.com/display/SUPOPS/New+Trending+Report (Jiras that are already created for trending items)
+Trends_filter='jql=filter%3D100089&fields=customfield_10302,key'
+
+#All FGCs in the past 24-42 hours
+Daily_filter='jql=filter%3D100745&fields=customfield_10302,key&maxResults=100'
+
+#All FGCs for the past Month
+Thirty_filter='jql=filter%3D91177&fields=customfield_10302,key&maxResults=1000'
+
+Non_Thirty_Filter = "issues/?jql=filter%3D91177%20and%20Customer%3D&maxResults=1000"
+
+#NON API Base URL
+jira2 = "https://jira2.workday.com/" 
+
+#Store trending Customers to compare 
+Customer =[]
+Possible_Trend = []
+Repeat = []
+
+#Store the jiras that might be trending so we can reference them if needed
+Issue = []
+
+#API requests based on the three queries (Daily_filter, Trends_Filter, Thirty_Filter) 
+response1=requests.request('GET', jira_server+Trends_filter, verify=False, auth=(username,password))
+Trends_Response = json.loads(response1.text)
+
+response2=requests.request('GET', jira_server+Daily_filter, verify=False, auth=(username,password))
+Daily_Response = json.loads(response2.text)
+
+response3=requests.request("GET", jira_server+Thirty_filter, verify=False, auth=(username,password))
+Monthly_Response = json.loads(response3.text)
+
+
+
+#Go Through all the issues That are trending
+for a in Trends_Response['issues']:
+
+    #Check if we have stored them already
+    if a['fields']['customfield_10302'][0]['value'] not in Customer:
+        #Store trending customers to reference them later
+        Customer.append(a['fields']['customfield_10302'][0]['value'])
+    
+
+
+
+#Go Through the issues for the past 42 hours
+for item in Daily_Response['issues']:
+
+    #Check the Trends Again (This could be easier but this line works so I don't want to change it.) 
+    for stuff in Trends_Response['issues']:
+        # Check the there is an issue the past 42 hours that has a customer already trending  
+        if item['fields']['customfield_10302'][0]['id'] == stuff['fields']['customfield_10302'][0]['id']:
+                #Lets link this jira to the trending jira
+                print("Already trend:", item['key'], " ", stuff['fields']['customfield_10302'][0]['value'], jira2, "browse/", stuff['key'],  "\n", sep='')
+                
+    #Lets open the Issues over the past month
+    for i in Monthly_Response['issues']:
+
+        #Has this customer seen an issue the past 30 days? 
+        if item['fields']['customfield_10302'][0]['id'] == i['fields']['customfield_10302'][0]['id']:
+            #Rule out that it is this same jira as this one
+            if item['key'] != i['key']:
+                #Have we already checked this jira?
+                if item['key'] not in Issue:
+                    Issue.append(item['key'])
+
+                    #Make sure they are not Trending already
+                    if item['fields']['customfield_10302'][0]['value'] not in Customer:
+                        if item['fields']['customfield_10302'][0]['value'] not in Possible_Trend:
+                            Possible_Trend.append(item['fields']['customfield_10302'][0]['value'])
+
+                
+
+for i in Possible_Trend:
+    print("Customer:",i, "is possibly a Trend. Here is a list of issues That has happend over the past month: ")
+
+    for b in Monthly_Response['issues']:
+
+        if i == b['fields']['customfield_10302'][0]['value']:
+              print(b['key'])
+
+
+            
+            
+        
+
+
+        
+        
